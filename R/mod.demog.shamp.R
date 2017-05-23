4f26faca140e2b10ea0d4f1d5d8acaa8f9883db7
dth2 <dth2@u.washington.edu>
2017-05-23 07:51:30 -0700

    temportary module to track the demographic composition of the network a multiple time steps.

---
+++ R/mod.demog.shamp.R
@@ -0,0 +1,20 @@
+
+#' @title Demography Check Module
+#'
+#' @description Module track demographic distribution over time.
+#'
+#' @inheritParams aging_het
+#'
+#' @keywords module het
+#'
+#' @export
+#'
+demog_shamp <- function(dat, at) {
+
+ if(at==4){demog4<<-table(dat$attr$demog.cat)}
+ if(at==52){demog52<<-table(dat$attr$demog.cat)}
+ if(at==104){demog104<<-table(dat$attr$demog.cat)}
+ if(at==1040){demog1040<<-table(dat$attr$demog.cat)} 
+
+  return(dat)
+}
