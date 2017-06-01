32b5460b6037687e94d44b32cd70171486f70f08
dth2 <dth2@u.washington.edu>
2017-06-01 09:51:30 -0700

    temporary function to track consistency of demographics over time

--- R/mod.demog.shamp.R
+++ R/mod.demog.shamp.R
@@ -14,7 +14,10 @@ demog_shamp <- function(dat, at) {
  if(at==4){demog4<<-table(dat$attr$demog.cat)}
  if(at==52){demog52<<-table(dat$attr$demog.cat)}
  if(at==104){demog104<<-table(dat$attr$demog.cat)}
- if(at==1040){demog1040<<-table(dat$attr$demog.cat)} 
+ if(at==208){demog208<<-table(dat$attr$demog.cat)}
+ if(at==312){demog312<<-table(dat$attr$demog.cat)}
+ if(at==416){demog416<<-table(dat$attr$demog.cat)}
+ if(at==520){demog520<<-table(dat$attr$demog.cat)} 
 
   return(dat)
 }
