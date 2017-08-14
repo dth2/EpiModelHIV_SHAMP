d7edf1b6f4dd3a451ea2f54e0c5c94c8e90aac58
dth2 <dth2@u.washington.edu>
2017-08-14 15:04:48 -0700

    NEw funtion to update demographic group attributes that change over time.

--- R/mod.demog.shamp.R
+++ R/mod.demog.shamp.R
@@ -1,7 +1,7 @@
 
 #' @title Demography Check Module
 #'
-#' @description Module track demographic distribution over time.
+#' @description Module updated demographic clasifications that change over time.
 #'
 #' @inheritParams aging_het
 #'
@@ -9,15 +9,92 @@
 #'
 #' @export
 #'
-demog_shamp <- function(dat, at) {
+demogupdate_shamp <- function(dat, at) {
 
- if(at==4){demog4<<-table(dat$attr$demog.cat)}
- if(at==52){demog52<<-table(dat$attr$demog.cat)}
- if(at==104){demog104<<-table(dat$attr$demog.cat)}
- if(at==208){demog208<<-table(dat$attr$demog.cat)}
- if(at==312){demog312<<-table(dat$attr$demog.cat)}
- if(at==416){demog416<<-table(dat$attr$demog.cat)}
- if(at==520){demog520<<-table(dat$attr$demog.cat)} 
- if(at==1040){demog1040<<-table(dat$attr$demog.cat)}
+
+  #AGECAT
+  dat$attr$agecat<-ifelse(data$att$age < 26 ,"18-25",
+                           ifelse(data$attr$age > 25 & data$attr$age < 36,"26-35",
+                                  ifelse(data$attr$age > 35 & data$attr$age < 46,"36-45",
+                                         ifelse(data$attr$age > 45,"46-60",data$attr$agecat))))
+  
+  #sqrt.age.adj
+  dat$attr$sqrt.age.adj<-ifelse(dat$attr$sex=="M",dat$attr$sqrt.age,
+                                ifelse(dat$attr$sex=="F",dat$attr$sqrt.age + dat$param$age.adj,dat$attr$sqrt.age.adj))
+  
+  #Create a new factor for age by sex group by pers.c.
+  
+  dat$attr$race.sex.pers<-rep(NA,length(dat$attr$age)) 
+  dat$attr$race.sex.pers<-ifelse(dat$attr$race=="B" & dat$attr$sex=="M" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="BI" & dat$attr$sex=="M" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="H" & dat$attr$sex=="M" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="HI" & dat$attr$sex=="M" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="W" & dat$attr$sex=="M" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="B" & dat$attr$sex=="F" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="BI" & dat$attr$sex=="F" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="H" & dat$attr$sex=="F" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="HI" & dat$attr$sex=="F" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="W" & dat$attr$sex=="F" & dat$attr$deg.pers.c==0,"Ref.0",
+                                  ifelse(dat$attr$race=="B" & dat$attr$sex=="M" & dat$attr$deg.pers.c==1,"B.M.p1",
+                                  ifelse(dat$attr$race=="BI" & dat$attr$sex=="M" & dat$attr$deg.pers.c==1,"BI.M.p1",
+                                  ifelse(dat$attr$race=="H" & dat$attr$sex=="M" & dat$attr$deg.pers.c==1,"H.M.p1",
+                                  ifelse(dat$attr$race=="HI" & dat$attr$sex=="M" & dat$attr$deg.pers.c==1,"HI.M.p1",
+                                  ifelse(dat$attr$race=="W" & dat$attr$sex=="M" & dat$attr$deg.pers.c==1,"W.M.p1",
+                                  ifelse(dat$attr$race=="B" & dat$attr$sex=="F" & dat$attr$deg.pers.c==1,"B.F.p1",
+                                  ifelse(dat$attr$race=="BI" & dat$attr$sex=="F" & dat$attr$deg.pers.c==1,"BI.F.p1",
+                                  ifelse(dat$attr$race=="H" & dat$attr$sex=="F" & dat$attr$deg.pers.c==1,"H.F.p1",
+                                  ifelse(dat$attr$race=="HI" & dat$attr$sex=="F" & dat$attr$deg.pers.c==1,"HI.F.p1",
+                                  ifelse(dat$attr$race=="W" & dat$attr$sex=="F" & dat$attr$deg.pers.c==1,"W.F.p1",
+                                  dat$attr$race.sex.pers))))))))))))))))))))
+  
+  
+  
+  #Create a new factor for age by sex group by main.c 
+  dat$attr$race.sex.cohab<-rep(NA,length(dat$attr$age))
+  dat$attr$race.sex.cohab<-ifelse(dat$attr$race=="B" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="BI" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="H" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="HI" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="W" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="B" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="BI" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="H" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="HI" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="W" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==0,"Ref.0",
+                                   ifelse(dat$attr$race=="B" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==1,"B.M.p1",
+                                   ifelse(dat$attr$race=="BI" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==1,"BI.M.p1",
+                                   ifelse(dat$attr$race=="H" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==1,"H.M.p1",
+                                   ifelse(dat$attr$race=="HI" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==1,"HI.M.p1",
+                                   ifelse(dat$attr$race=="W" & dat$attr$sex=="M" & dat$attr$deg.cohab.c==1,"W.M.p1",
+                                   ifelse(dat$attr$race=="B" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==1,"B.F.p1",
+                                   ifelse(dat$attr$race=="BI" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==1,"BI.F.p1",
+                                   ifelse(dat$attr$race=="H" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==1,"H.F.p1",
+                                   ifelse(dat$attr$race=="HI" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==1,"HI.F.p1",
+                                   ifelse(dat$attr$race=="W" & dat$attr$sex=="F" & dat$attr$deg.cohab.c==1,"W.F.p1",
+                                   dat$attr$race.sex.cohab))))))))))))))))))))
+  
+  
+  
+  #  Recalculate the demog.cat for new demo.
+  
+  sex.groups<-sort(unique(dat$attr$sex))
+  for (i in 1:(length(sex.groups))){
+    dat$attr$demog.cat<-ifelse(dat$attr$sex==sex.groups[i],i*1000,dat$attr$demog.cat)      
+  }
+  
+  race.groups<-sort(unique(dat$attr$race))
+  for (i in 1:(length(race.groups))){
+    dat$attr$demog.cat<-ifelse(dat$attr$race==race.groups[i],dat$attr$demog.cat+(i*100),dat$attr$demog.cat)      
+  }
+  
+  age.groups<-sort(unique(floor(dat$attr$age)))
+  age.temp<-floor(dat$attr$age)
+  for (i in 1:(length(age.groups))){   
+    dat$attr$demog.cat<-ifelse (age.temp==age.groups[i],dat$attr$demog.cat+(age.groups[i]),dat$attr$demog.cat)      
+  }
+  
+  
+  
+  
   return(dat)
 }
